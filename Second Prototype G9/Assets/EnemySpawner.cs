986084c96cbc64caaf9d54eb296b3badec12f59c
bong <30470346+oreoadidas@users.noreply.github.com>
2018-02-15 19:57:19 +0000

    v1

---
+++ Second Prototype G9/Assets/EnemySpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject Enemy; //enemy prefab
+
+    public float maxSpawnRate = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("SpawnEnemy", maxSpawnRate);
+
+        //increase spawn rate every 30 seconds
+        InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //end of screen
+
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //end of screen
+
+        GameObject anEnemy = (GameObject)Instantiate(Enemy);
+        anEnemy.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+
+        ScheduleNextEnemySpawn();
+    }
+
+    void ScheduleNextEnemySpawn()
+    {
+        float spawnInNSeconds;
+
+        if (maxSpawnRate > 1f)
+        {
+            spawnInNSeconds = Random.Range(1f, maxSpawnRate);
+        }
+        else
+            spawnInNSeconds = 1f;
+        Invoke("SpawnEnemy", spawnInNSeconds);
+    }
+
+    //difficulty increase:
+    void IncreaseSpawnRate()
+    {
+        if (maxSpawnRate > 1f)
+            maxSpawnRate--;
+        if (maxSpawnRate == 1f)
+            CancelInvoke("IncreaseSpawnRate");
+    }
+}
---
+++ Second Prototype G9/Assets/GcuHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GcuHealth : MonoBehaviour
+{
+    public float gcuHealth = 10f;
+    public bool lazy; 
+    public bool gameOver = false;
+    public int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool lazy = EnemyScript.laziness;
+
+        if (gameOver == true)
+        {
+            Application.LoadLevel("EndScene");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "EnemyShipTag")
+        {
+            if (lazy == true)
+            {
+                gcuHealth -= 1.0f;
+            }
+
+            if (lazy == false)
+            {
+                score++;
+            }
+        }
+    }
+
+    void GcuFail()
+    {
+        if (gcuHealth <= 0f)
+        {
+            gameOver = true;
+        }
+    }
+}
